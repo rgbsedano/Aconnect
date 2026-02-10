012a9f98d58994b1b545547d9f51e98601ba1bdb
rgbsedano <rgbsedano@gmail.com>
2026-02-10 13:30:43 +0800

    Create log-2026-02-10.php

---
+++ application/logs/log-2026-02-10.php
@@ -0,0 +1,31 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-02-10 06:30:29 --> $config['composer_autoload'] is set to TRUE but C:\xampp\htdocs\Aconnect_ci3\application\vendor/autoload.php was not found.
+INFO - 2026-02-10 06:30:29 --> Config Class Initialized
+INFO - 2026-02-10 06:30:29 --> Hooks Class Initialized
+DEBUG - 2026-02-10 06:30:29 --> UTF-8 Support Enabled
+INFO - 2026-02-10 06:30:29 --> Utf8 Class Initialized
+INFO - 2026-02-10 06:30:29 --> URI Class Initialized
+DEBUG - 2026-02-10 06:30:30 --> No URI present. Default controller set.
+INFO - 2026-02-10 06:30:30 --> Router Class Initialized
+INFO - 2026-02-10 06:30:30 --> Output Class Initialized
+INFO - 2026-02-10 06:30:30 --> Security Class Initialized
+DEBUG - 2026-02-10 06:30:30 --> Global POST, GET and COOKIE data sanitized
+INFO - 2026-02-10 06:30:30 --> Input Class Initialized
+INFO - 2026-02-10 06:30:30 --> Language Class Initialized
+INFO - 2026-02-10 06:30:30 --> Loader Class Initialized
+INFO - 2026-02-10 06:30:30 --> Helper loaded: url_helper
+INFO - 2026-02-10 06:30:30 --> Helper loaded: file_helper
+INFO - 2026-02-10 06:30:30 --> Helper loaded: html_helper
+INFO - 2026-02-10 06:30:31 --> Database Driver Class Initialized
+INFO - 2026-02-10 06:30:31 --> Session: Class initialized using 'files' driver.
+INFO - 2026-02-10 06:30:31 --> Helper loaded: form_helper
+INFO - 2026-02-10 06:30:31 --> Form Validation Class Initialized
+INFO - 2026-02-10 06:30:31 --> User Agent Class Initialized
+INFO - 2026-02-10 06:30:31 --> Language file loaded: language/english/pagination_lang.php
+INFO - 2026-02-10 06:30:31 --> Pagination Class Initialized
+INFO - 2026-02-10 06:30:31 --> Controller Class Initialized
+INFO - 2026-02-10 06:30:31 --> Model "M_login" initialized
+INFO - 2026-02-10 06:30:32 --> File loaded: C:\xampp\htdocs\Aconnect_ci3\application\views\login.php
+INFO - 2026-02-10 06:30:32 --> Final output sent to browser
+DEBUG - 2026-02-10 06:30:32 --> Total execution time: 2.8103
